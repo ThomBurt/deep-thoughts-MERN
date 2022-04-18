6b6028a9bc2846536f882d3c73d220db64bcb48e
ThomBurt <93936296+ThomBurt@users.noreply.github.com>
2022-04-18 16:21:21 -0400

    D

---
+++ README.md
@@ -0,0 +1,24 @@
+  addUser(username:"thom", password:"testThom", email:"thom@test.com") {
+
+      "_id": "625db9953b2bf95cf4a909ef",
+      "username": "thom",
+      "email": "thom@test.com"
+
+      "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InVzZXJuYW1lIjoidGhvbSIsImVtYWlsIjoidGhvbUB0ZXN0LmNvbSIsIl9pZCI6IjYyNWRiOTk1M2IyYmY5NWNmNGE5MDllZiJ9LCJpYXQiOjE2NTAzMTEwODUsImV4cCI6MTY1MDMxODI4NX0.45UHkpRtpQJQEto6xXQJKA4lFvOAjaWBUkFThXUheYU"
+
+======================================================================
+
+{
+  "username": "tester2",
+  "password": "test12345",
+  "email": "test2@test.com"
+}
+{
+  "data": {
+    "addUser": {
+      "_id": "625db9dd3b2bf95cf4a909f1",
+      "username": "tester2",
+      "email": "test2@test.com"
+    }
+  }
+}
--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const { authMiddleware } = require('./utils/auth');
+// import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
 
+// import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
@@ -8,16 +11,25 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 const startServer = async () => {
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers
+  // create a new Apollo server and pass in our schema data
+  const server = new ApolloServer({ 
+    typeDefs, 
+    resolvers, 
+    context: authMiddleware 
   });
+
+  // Start the Apollo server
   await server.start();
+
+  // integrate our Apollo server with the Express application as middleware
   server.applyMiddleware({ app });
+
+  // log where we can go to test our GQL API
   console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 };
 
-startServer()
+// Initialize the Apollo server
+startServer();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
